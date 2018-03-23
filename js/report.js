9b4cd01f819a19b9fc05dede35478c31cccdb046
vgulaev <vgulaev@yandex.ru>
2018-03-23 21:51:53 +0500

    Up

--- js/report.js
+++ js/report.js
@@ -19,9 +19,11 @@ function fillReport() {
     d[key][a] = localStorage.getItem(localStorage.key(i));
   }
   //var dd = Object
+  var total_hour = 0;
   Object.keys(d).sort().forEach( function (x) {
     var el = document.createElement("div");
     el.classList.add('divTableRow');
+    total_hour += parseInt(d[x]['ms']);
     //el.innerHTML = `<span>${x}==<span><span>${d[x]["k"]}<span>`;
     el.innerHTML = `<div class="divTableCell">${x}</div>
         <div class="divTableCell">${(d[x]['ms']/1000/60).toFixed(0)}</div>
@@ -29,6 +31,16 @@ function fillReport() {
         <div class="divTableCell">${(d[x]['k']/d[x]['ms'] * 1000 * 60).toFixed(1)}</div>`;
     body.append(el);
   })
+  var el = document.createElement("div");
+  el.classList.add('divTableRow');
+  minute = (total_hour/1000/60 % 60).toFixed(0);
+  total_hour = total_hour/1000/60/60;
+
+  el.innerHTML = `<div class="divTableCell">Total:</div>
+      <div class="divTableCell">${total_hour.toFixed(0)} h ${minute} m</div>
+      <div class="divTableCell"></div>
+      <div class="divTableCell"></div>`;
+  body.append(el);
 }
 
 window.addEventListener("load", async function( event ) {
