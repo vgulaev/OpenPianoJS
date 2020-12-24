3cb1cf1d1a86f4d54af26ad11ca9585cce93872f
vgulaev <vgulaev@yandex.ru>
2020-12-24 22:25:11 +0500

    Up

--- js/v4/classes/xml/timePoint.js
+++ js/v4/classes/xml/timePoint.js
@@ -23,10 +23,32 @@ export class TimePoint {
     this.clefs[el.number] = el;
   }
 
+  key(el) {
+    if (!this.fifths) {
+      this.fifths = el;
+    }
+  }
+
+  time(el) {
+    if (!this.timeSignature) {
+      this.timeSignature = el;
+    }
+  }
+
   draw(pm) {
     if (this.clefs) {
       Object.keys(this.clefs).forEach(c => this.clefs[c].draw(pm));
       pm.cursor += 50;
+    }
+    if (this.fifths) {
+      let g = SVGBuilder.keySignature(pm.drawClefs[1].toS(), pm.drawClefs[2].toS(), this.fifths.fifths, pm.cursor);
+      pm.g.append(g);
+      pm.cursor += Math.abs(this.fifths.fifths) * 14 + 10;
+    }
+    if (this.timeSignature) {
+      let g = SVGBuilder.setTimeSignature(this.timeSignature['beats'], this.timeSignature['beat-type'], pm.cursor);
+      pm.g.append(g);
+      pm.cursor += 40;
     }
     let t = SVGBuilder.text({x: pm.cursor, y: 71, text: 'TP'});
     pm.g.append(t);
