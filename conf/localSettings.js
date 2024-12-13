3144559fa607ef890c0364b15fb86c1eee8d5255
vgulaev <vgulaev@yandex.ru>
2024-12-13 17:08:28 +0500

    Up

---
+++ conf/localSettings.js
@@ -0,0 +1,281 @@
+export class Settings {
+
+}
+
+// Settings.coach = function (a, m, s) {
+//   // return (new PlayFaster(a, m, s));
+//   // return (new PlayBlind(a, m, s));
+
+//   //****************************************
+//   // Metronome.disable();
+//   // var coach = new PlayRepeat(App.piano, md, Settings.range);
+//   //return (new Play10timesRule(a, m, s));
+//   //return (new PlayFromSlownest(a, m, s));
+//   //return (new PlayErrorCleaner(a, m, s));
+//   // return (new PlayChordFromSheet(a, m, s));
+//   //return (new PlayFive(a, m, ''));
+//   //return (new PlayTriplets(a, m));
+// }
+
+Settings.loadDefault = () => {
+  // app.load('data/xml/Aaron Zigman The Notebook Theme.musicxml');
+  // app.load('data/xml/Adagio in G minor (Giazotto Albinoni).musicxml');
+  // app.load('data/xml/Amnesia A Machine For Pigs - Mandus.musicxml');
+  // app.load('data/xml/Albeniz - Asturias Suite Espaola No. 1.musicxml');
+  // app.load('data/xml/Arpeggio D minor broken.musicxml');
+  // app.load('data/xml/A_Thousand_Miles.musicxml');
+  // app.load('data/xml/Bach G Minor arranged Luo Ni.musicxml');
+  // app.load('data/xml/Beethoven Bagatelle No. 25 in A minor.musicxml');
+  // app.load('data/xml/Beethoven Piano_Sonata_No.21_Op.53.musicxml');
+  // app.load('data/xml/Beethoven - Sonate No. 2 2nd Movement.musicxml');
+  // app.load('data/xml/Beethoven Sonata No. 8 Op. 13 Pathetique.musicxml');
+  // app.load('data/xml/Beethoven Sonata No. 8 Op. 13 Pathetique III Rondo.musicxml');
+  // app.load('data/xml/Beethoven Sonata 14 C Sharp Minor.musicxml');
+  // app.load('data/xml/Beethoven Sonata 14 C Sharp Minor 2rd Movemen.musicxml');
+  app.load('data/xml/Beethoven Sonata 14 C Sharp Minor 3rd Movemen.musicxml');
+  // app.load('data/xml/Beethoven Sonata No. 8 Op. 13 Pathetique - Second Ab major.musicxml');
+  // app.load('data/xml/Beethoven Sonate No. 17 Tempest 3rd Movement.musicxml');
+  // app.load('data/xml/Beethoven Sonata No. 23 Op. 57 Appassionata Complete.musicxml');
+  // app.load('data/xml/Beethoven Symphony No. 5 1 st movement Piano solo.musicxml');
+  // app.load('data/xml/Beethoven - Symphony 7 II Allegretto.musicxml');
+  // app.load('data/xml/billie jean.musicxml');
+  // app.load('data/xml/Brahms Op. 35 Variations on a Theme Paganini.musicxml');
+  // app.load('data/xml/Brahms - Hungarian Dance No.5 EASY version Flowkey.musicxml');
+  // app.load('data/xml/Brahms - Hungarian Dance No 5 in G Minor.musicxml');
+  // app.load('data/xml/Brahms - Lullaby easy.musicxml');
+  // app.load('data/xml/Britney Spears - Everytime.musicxml');
+  // app.load('data/xml/Burgmuller - Op.100 - Arabesque.musicxml');
+  // app.load('data/xml/BWV Anh Menuet 3.musicxml');
+  // app.load('data/xml/BWV - Bach_Musette_in_D_Anh._126.musicxml');
+  // app.load('data/xml/BWV 528_Organ_Sonata_No._4_-_2._Andante_Adagio_-_Vikingur_Olafsson_Interpretation.musicxml');
+  // app.load('data/xml/BWV 543 Prelude A Minor.musicxml');
+  // app.load('data/xml/BWV-565.musicxml');
+  // app.load('data/xml/BWV 578 - Bach Fugue in G Minor Piano solo.musicxml');
+  // app.load('data/xml/BWV 830 Partita E minor.musicxml');
+  // app.load('data/xml/BWV 847 Prelude C Minor.musicxml');
+  // app.load('data/xml/BWV 847 Fugue in C Minor.musicxml');
+  // app.load('data/xml/BWV 848 Prelude C Major.musicxml');
+  // app.load('data/xml/BWV 1068 J._S._Bach_-_Air_on_the_G_String_Piano_arrangement.musicxml');
+  // app.load('data/xml/BWV 849 Prelude C sharp Minor.musicxml');
+  // app.load('data/xml/BWV 851 Prelude D Minor.musicxml');
+  // app.load('data/xml/BWV 853 Prelude N8 in E Flat Minor.musicxml');
+  // app.load('data/xml/BWV 855 Prelude No. 10 in E Minor.musicxml');
+  // app.load('data/xml/BWV 855a Prelude_in_B_minor_arr._by_A_Siloti.musicxml');
+  // app.load('data/xml/BWV-858_Prelude_F_sharp_Major.xml');
+  // app.load('data/xml/BWV 927.musicxml');
+  // app.load('data/xml/BWV 939 Prelude No. 2 in C Major.musicxml');
+  // app.load('data/xml/BWV 940 Prelude D minor.musicxml');
+  // app.load('data/xml/BWV 974 - Adagio Concerto D minor.musicxml');
+  // app.load('data/xml/BWV 1004 Chaconne by Busoni-Bach.musicxml');
+  // app.load('data/xml/BWV 1052 Keyboard Concerto No. 1 in D minor.musicxml');
+  // app.load('data/xml/BWV 1067 J.S._Bach_Badinerie_aus_der_Orchestersuite_Nr_.2_h-Moll.musicxml');
+  // app.load('data/xml/Canon_in_D_full.musicxml');
+  // app.load('data/xml/Carol of the Bells.musicxml');
+  // app.load('data/xml/Chopin - Fantaisie-Impromptu.musicxml');
+  // app.load('data/xml/Chopin 25-11 in A Minor Winter Wind.musicxml');
+  // app.load('data/xml/Chopin 28-2 in A Minor.musicxml');
+  // app.load('data/xml/Chopin 28-14 in Eb Minor.musicxml');
+  // app.load('data/xml/Chopin 28-15 in Db Major.musicxml');
+  // app.load('data/xml/Chopin Op. 64 No. 1.musicxml');
+  // app.load('data/xml/Chopin op. 69 No. 2 in B Minor.musicxml');
+  // app.load('data/xml/Chopin Sonata_No.2_in_B-flat_minor.musicxml'); // (210 - лирическая часть)
+  // app.load('data/xml/Chopin_-_Nocturne_Op_9_No_1_B_Flat_Minor.musicxml');
+  // app.load('data/xml/Chopin_Nocture_opus_9_number2.musicxml');
+  // app.load('data/xml/Chopin Nocturne in C sharp Minor.musicxml');
+  // app.load('data/xml/Chopin Drei Walzer Op. 64 No. 2.musicxml');
+  // app.load('data/xml/Chromatic - exercise.musicxml');
+  // app.load('data/xml/Clint Mansell - Requiem For a Dream Pianistos Eazy.musicxml');
+  // app.load('data/xml/Czerny- Op.718 - 17.musicxml');
+  // app.load('data/xml/Czerny-Germer-19.musicxml');
+  // app.load('data/xml/Czerny - 50 little Studies 01.musicxml');
+  // app.load('data/xml/Czerny - 50 little Studies 04.musicxml');
+  // app.load('data/xml/Czerny - Op. 740 Etude in C major no. 1.musicxml');
+  // app.load('data/xml/Czerny - Op. 740 Etude in G minor no. 14.musicxml');
+  // app.load('data/xml/Czerny - Op. 740 Etude in G minor no. 50.musicxml');
+  // app.load('data/xml/D dur.musicxml');
+  // app.load('data/xml/Debussy Claude L.113 Le Coin des enfants - La neige danse Childrens Corner - The Snow Is Dancing.musicxml');
+  // app.load('data/xml/Debussy - Clair de Lune Suite Bergamasque No. 3.musicxml');
+  // app.load('data/xml/Dance_of_the_Knights_Transcription_for_solo_piano.musicxml');
+  // app.load('data/xml/Domenico Scarlatti K.466 - E-F-G.musicxml');
+  // app.load('data/xml/Domenico Scarlatti K.466.musicxml');
+  // app.load('data/xml/Einaudi - Fly.musicxml');
+  // app.load('data/xml/Etude_No._15_in_D_Minor_Opus_45_No._15_Heller.musicxml');
+  // app.load('data/xml/Ennio Morricone - Chi Mai.musicxml');
+  // app.load('data/xml/Evil_Morty_Theme_For_the_Damaged_Coda_Solo_Piano.musicxml');
+  // app.load('data/xml/Game_of_Thrones_Main_Theme.musicxml');
+  // app.load('data/xml/Greensleeves.musicxml');
+  // app.load('data/xml/Gnossienne_No._1.musicxml');
+  // app.load('data/xml/Gymnopedie.musicxml');
+  // app.load("data/xml/John Towner Williams - Hedwig's theme.musicxml")
+  // app.load('data/xml/Grieg - Op.43 Lyric Pieces No. 6 - To Spring.musicxml');
+  // app.load('data/xml/Grieg - Op.57 No.6 - Hjemve Homesickness Nostalgia di casa.musicxml');
+  // app.load('data/xml/Grieg - Op.71 Kobold.musicxml');
+  // app.load('data/xml/Grieg In The Hall Of The Mountain King.musicxml');
+  // app.load('data/xml/Grieg_-_Piano_Concerto_in_A_minor_-_1st_movement_-_Piano_Solo.musicxml');
+  // app.load('data/xml/Hallelujah__Leonard_Cohen_Rousseau_Cover.musicxml');
+  // app.load('data/xml/Halloran - The Lion theme.musicxml');
+  // app.load('data/xml/Halloran Dustin - Opus No. 17.musicxml');
+  // app.load('data/xml/Handel - Passacaglia_from_Suite_no.7_HWV_432_-_Georg_Frideric.musicxml');
+  // app.load('data/xml/Hanon - Ex 6.musicxml');
+  // app.load('data/xml/Hanz Zimmer - Mountains.musicxml');
+  // app.load('data/xml/Hanz Zimmer - Pirates_of_the_Caribbean - Hes a Pirate.musicxml');
+  // app.load('data/xml/Happy Birthday To You Piano.musicxml');
+  // app.load('data/xml/Ilya Beshelvi - Wind.musicxml');
+  // app.load('data/xml/Jingle_Bells.musicxml');
+  // app.load('data/xml/John Cage - In a Landscape.musicxml');
+  // app.load('data/xml/John Field - Nocturne No.5 - H.37 Edited by Liszt.musicxml');
+  //app.load('data/xml/John Field - Nocturne No. 10 in E minor H 46.musicxml');
+  // app.load('data/xml/LE_COUCOU_by_DAQUIN_The_Cuckoo.musicxml');
+  // app.load('data/xml/Liszt - Etude_in_G_Minor_La_Campanella_S._1413.musicxml');
+  // app.load('data/xml/Liszt S. 136 No.10.musicxml');
+  // app.load('data/xml/Liszt - Etude_in_G_Minor_La_Campanella_S._1413-cuted.musicxml');
+  // app.load('data/xml/Liszt S. 136 No.4.musicxml');
+  // app.load('data/xml/Lucifer - Creep.musicxml');
+  // app.load('data/xml/Ludovico Einaudi - Dietro Casa.musicxml');
+  // app.load('data/xml/Maksim Mrvica - Croatian Rhapsody.musicxml');
+  // app.load('data/xml/Max Richter - The Departure The Leftovers score.musicxml');
+  // app.load('data/xml/May I See U Again.musicxml');
+  // app.load('data/xml/Mendelssohn - Venetian Gondola Song No. 6 op. 19.musicxml');
+  // app.load('data/xml/Metallica - Nothing Else Matters.musicxml');
+  // app.load('data/xml/Moonlight serenade.musicxml');
+  // app.load('data/xml/Mozart - Piano Concerto No. 20 K.466 1st Mvt.musicxml');
+  // app.load('data/xml/Mozart - Lacrimosa_original_transcription_for_piano.musicxml');
+  // app.load('data/xml/Mozart - Piano Sonata No.11 in A major-K.331W.musicxml');
+  // app.load('data/xml/Mozart - Piano_Sonata_No._11_K._331_3rd_Movement_Rondo_alla_Turca.musicxml');
+  // app.load('data/xml/Mozart K.488_Piano_Concerto_23_in_A_2nd_mov._Adagio_for_piano_solo.musicxml');
+  // app.load('data/xml/Mozart-Fantasia in D Minor K. 397.musicxml');
+  // app.load('data/xml/Mozart Fantasia in C minor K. 475.musicxml');
+  // app.load('data/xml/Mozart - K.265 - 12_Variations_of_Twinkle_Twinkle_Little_Star.musicxml');
+  // app.load('data/xml/Mozart-Sonata_16.musicxml');
+  // app.load('data/xml/Mozart-Sonata_16_2nd_movement.musicxml');
+  // app.load('data/xml/Mussorgsky - Bydlo.musicxml');
+  // app.load('data/xml/Mussorgsky - Gnomus.musicxml');
+  // app.load('data/xml/Mussorgsky - Promenade.musicxml');
+  // app.load('data/xml/My Heart Will Go On.musicxml');
+  // app.load('data/xml/Myuu - Identity Crisis.musicxml');
+  // app.load('data/xml/Dominant.musicxml');
+  // app.load('data/xml/My first compose.musicxml');
+  // app.load('data/xml/National_Anthem_of_the_USSRRussia.musicxml');
+  // app.load('data/xml/Nutcracker 14a Pas de Deux Hinci version.musicxml');
+  // app.load('data/xml/Nino Rota - The Godfather.musicxml');
+  // app.load('data/xml/Nino Rota - The Godfather C minor.musicxml');
+  // app.load('data/xml/Paganini 24 Caprice.musicxml');
+  // app.load('data/xml/Paul Mauriat - Toccata.musicxml');
+  // app.load('data/xml/Philip Glass - Walk to school.musicxml');
+  // app.load('data/xml/Prelude_in_G_Minor_Op._23_No._5.musicxml');
+  // app.load('data/xml/Prelude_Opus_28_No._4_in_E_Minor.musicxml');
+  // app.load('data/xml/primavera.musicxml');
+  // app.load('data/xml/Rachmaninoff - op.10 - 6 Romance.musicxml');
+  // app.load('data/xml/Rachmaninoff_-_Piano_Concerto_No._2_Op._18.musicxml');
+  // app.load('data/xml/Rachmaninoff - Prelude in C-sharp Minor Opus 3 No. 2.musicxml');
+  // app.load('data/xml/Rachmaninoff - Elegie Opus 3 No. 1.musicxml');
+  // app.load('data/xml/Rachmaninoff - Flight of the Bumblebee - N. Rimsky-Korsakov.musicxml');
+  // app.load('data/xml/Rachmaninoff - Preludes Op. 32 No. 12 in G-sharp minor.musicxml');
+  // app.load('data/xml/Re major arpeggio.musicxml');
+  // app.load('data/xml/River_Flows_in_You.musicxml');
+  // app.load('data/xml/Rob Dougan - Clubbed to Death.musicxml');
+  // app.load('data/xml/Sarabande_de_Haendel.musicxml');
+  // app.load('data/xml/Scale D Major diverge.musicxml');
+  // app.load('data/xml/Schubert Serenade - Standchen - Arr. By Lizst D 957 №4.musicxml');
+  // app.load('data/xml/Schubert_Ellens Gesang III D.839.musicxml');
+  // app.load('data/xml/Schumann - Kinderszenen - No.7 Op.15 - Dreaming Trumerei.musicxml');
+  // app.load('data/xml/Schumann - Symphonic Etudes Op.13 Etude I Var. 1.musicxml');3
+  // app.load('data/xml/Schumann - The Wild Horseman Op. 68 No. 8.musicxml');
+  // app.load('data/xml/Schumann_Toccata_Op._7_Piano_solo.musicxml');
+  // app.load('data/xml/Secret Garden - Song From a Secret Garden.musicxml');
+  // app.load('data/xml/Shostakovich_Waltz_No._2.musicxml');
+  // app.load('data/xml/Shostakovich - Piano_Concerto_No._2_in_F_Major_Op._102_II._Andante.musicxml');
+  // app.load('data/xml/Sibelius Jean - Etude op. 76 n.2.musicxml');
+  // app.load('data/xml/Sorry Seems To Be The Hardest Word.musicxml');
+  // app.load('data/xml/SYML - I wanted to Leave.musicxml');
+  // app.load('data/xml/SYML - Mr._Sandman.musicxml');
+  // app.load('data/xml/Tchaikovsky - Op. 37 no. 3 March Song of the Lark.musicxml');
+  // app.load('data/xml/Tchaikovsky - Piano Concerto No 1.musicxml');
+  // app.load('data/xml/Tchaikovsky - Swan Lake.musicxml');
+  // app.load('data/xml/Tchaikovsky_Op._39_Childrens_Album_-_P._I._IX. Новая кукла.musicxml');
+  // app.load('data/xml/Tchaikovsky_Op._39_Childrens_Album_-_P._I._VI. Болезнь куклы.musicxml');
+  // app.load('data/xml/Tchaikovsky_Op._39_Childrens_Album_-_P._I._VII. Похороны куклы.musicxml');
+  // app.load('data/xml/Tchaikovsky_Op._39_Childrens_Album_-_P._I._XVI. Стариная француская песенка.musicxml');
+  // app.load('data/xml/Tchaikovsky_Op._39_Childrens_Album_-_P._I._XVIII. Неаполитанская песенка.musicxml');
+  // app.load('data/xml/Tchaikovskys - October Autumn Song.musicxml');
+  // app.load('data/xml/The Daydream - Tears B minor.musicxml');
+  // app.load('data/xml/The Daydream - Tears.musicxml');
+  // app.load('data/xml/The Entertainer.musicxml');
+  // app.load('data/xml/The pink panther theme.musicxml');
+  // app.load('data/xml/Umberto Tozzi - Ti Amo Piano from La Casa de Papel.musicxml');
+  // app.load('data/xml/Una Mattina.musicxml');
+  // app.load('data/xml/We_are_the_Champion.musicxml');
+  // app.load('data/xml/Woodsman - Dream River.musicxml');
+  // app.load('data/xml/Yehezkel Raz - Behind every decision.musicxml');
+  // app.load('data/xml/Воспоминание.musicxml');
+  // app.load('data/xml/Глинка - Двухголосная Фуга.musicxml');
+  // app.load('data/xml/Глиэр - Прелюдия Соч 43-1.musicxml');
+  // app.load('data/xml/Деак - Ну погоди.musicxml');
+  // app.load('data/xml/Десятников - Из жизни Кощея.musicxml');
+  // app.load('data/xml/Кабалевский - Кавалерийская.musicxml');
+  // app.load('data/xml/Кобылянский - Этюд 7.musicxml');
+  // app.load('data/xml/Крылатые качели.musicxml');
+  // app.load('data/xml/Оттепель - аранжировка Александр Лосев.musicxml');
+  // app.load('data/xml/Прокофьев - Piano Sonata No.6 Op.82.musicxml');
+  // app.load('data/xml/Прокофьев - Сказочка Op. 65 - 3.musicxml');
+  // app.load('data/xml/Скрябин op. 8 - No 12 in D Minor.musicxml');
+  // app.load('data/xml/Скрябин op. 11 - No 10 C sharp minor.musicxml');
+  // app.load('data/xml/Скрябин op. 42 - No 8 F sharp major.musicxml');
+  // app.load('data/xml/Чайковский - Танец феи драже (Плетнёв).musicxml');
+  // app.load('data/xml/Шостакович - Прелюдия IV. Окончательный.musicxml');
+  // app.load('data/xml/Vivaldi_-_Violin_Concerto_in_E_major_Op._8_No._1_RV._269_Spring_for_SoloPiano.musicxml');
+  // app.load('data/xml/Vivaldi Violin Concerto in F minor Op. 8 No. 4 RV. 297 Winter for Solo Piano.musicxml');
+  // app.load('data/xml/Yann Tiersen - Comptine Dun Autre Ete Lapres.musicxml');
+  // app.load('data/xml/Lonely_shepherd_composed_by_James_Last.musicxml');
+  // app.load('data/xml/Shchedrin Rodion - Humoresque.musicxml');
+  // app.load('data/xml/test.musicxml');
+}
+
+// Concerto №1
+// Settings.range = [0, 202];
+
+// Bogatelle 25
+// Settings.range = [263, 368];
+// Settings.range = [620, 672];
+
+// River flow
+// Settings.range = [261, 1000];
+// Settings.range = [22, 1000];
+// Settings.range = [100, 1000];
+// Settings.range = [8, 1000];
+
+// Yann Tiersen - Comptine Dun Autre Ete Lapres
+// Settings.range = [431, 751];
+//Settings.range = [750, 830];
+// Settings.range = [830, 910];
+
+// Grieg In The Hall Of The Mountain King
+// Settings.range = [159, 198];
+//Settings.range = [0, 198];
+
+// Dance of the Knights
+// Settings.range = [122, 132];
+
+//Settings.range = [471, 1210];
+
+//Settings.range = [145, 181];
+// Chopin - Drei Walzer Op. 64 No. 2
+//Settings.range = [159, 250];
+
+//Rahmaninoff
+// Settings.range = [0, 352];
+// Settings.range = [139, 2550];
+
+// 31 такт удалить последнюю четверть паузы.
+
+// 740
+// Settings.range = [2249, 100000];
+// Settings.range = [0, 63];
+// Settings.range = [0, 127];
+// Settings.range = [128, 255];
+// Settings.range = [528, 2550];
+// Settings.range = [381, 1120];
+// Settings.range = [253, 10000];
+
+// http://192.168.56.10/
+
